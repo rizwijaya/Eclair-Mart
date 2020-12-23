541d71caf6d7564f1c2495fe2a494ae6ddeefecb
inayawidya <73151978+inayawidya@users.noreply.github.com>
2020-12-23 23:57:27 +0700

    up to

--- application/views/home.php
+++ application/views/home.php
@@ -39,12 +39,12 @@
       </div>
       <!-- HERO SECTION-->
       <div class="container">
-          <section class="hero pb-3 bg-cover bg-center d-flex align-items-center" style="background: url(<?php echo base_url(); ?>/assets/assets_home/img/hero-banner-alt.jpg)">
+          <section class="hero pb-3 bg-cover bg-center d-flex align-items-center" style="background: url(<?php echo base_url(); ?>/assets/assets_home/img/beauty.jpg)">
               <div class="container py-5">
                   <div class="row px-4 px-lg-5">
                       <div class="col-lg-6">
                           <p class="text-muted small text-uppercase mb-2">Inspirasi Minimarket 2020</p>
-                          <h1 class="h2 text-uppercase mb-3">Diskon 20% Jam Alarm</h1><a class="btn btn-dark" href="shop.html">Cari Sekarang</a>
+                          <a class="btn btn-dark" href="shop.html">Cari Sekarang</a>
                       </div>
                   </div>
               </div>
