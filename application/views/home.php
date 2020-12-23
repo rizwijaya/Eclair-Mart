f045ebb2cbe618da622454824ca393982f81cb58
inayawidya <73151978+inayawidya@users.noreply.github.com>
2020-12-23 21:58:54 +0700

    up to

--- application/views/home.php
+++ application/views/home.php
@@ -81,8 +81,8 @@
                                   </ul>
                               </div>
                           </div>
-                          <h6> <a class="reset-anchor" href="detail.html">Kui Ye Chen’s AirPods</a></h6>
-                          <p class="small text-muted">$250</p>
+                          <h6> <a class="reset-anchor" href="detail.html">Meja Lipat Portable </a></h6>
+                          <p class="small text-muted">Rp70.000</p>
                       </div>
                   </div>
                   <!-- PRODUCT-->
@@ -98,8 +98,8 @@
                                   </ul>
                               </div>
                           </div>
-                          <h6> <a class="reset-anchor" href="detail.html">Air Jordan 12 gym red</a></h6>
-                          <p class="small text-muted">$300</p>
+                          <h6> <a class="reset-anchor" href="detail.html">Nestle Dancow FortiGro Instant Full Cream 800gr</a></h6>
+                          <p class="small text-muted">Rp67.000</p>
                       </div>
                   </div>
                   <!-- PRODUCT-->
@@ -115,8 +115,8 @@
                                   </ul>
                               </div>
                           </div>
-                          <h6> <a class="reset-anchor" href="detail.html">Cyan cotton t-shirt</a></h6>
-                          <p class="small text-muted">$25</p>
+                          <h6> <a class="reset-anchor" href="detail.html">Chicken Nugget Belfoods 250 gram</a></h6>
+                          <p class="small text-muted">Rp15.000</p>
                       </div>
                   </div>
                   <!-- PRODUCT-->
@@ -132,8 +132,8 @@
                                   </ul>
                               </div>
                           </div>
-                          <h6> <a class="reset-anchor" href="detail.html">Timex Unisex Originals</a></h6>
-                          <p class="small text-muted">$351</p>
+                          <h6> <a class="reset-anchor" href="detail.html">Kursi Olymplast Milenial Dacey Cessi</a></h6>
+                          <p class="small text-muted">Rp250.000</p>
                       </div>
                   </div>
                   <!-- PRODUCT-->
@@ -149,8 +149,8 @@
                                   </ul>
                               </div>
                           </div>
-                          <h6> <a class="reset-anchor" href="detail.html">Red digital smartwatch</a></h6>
-                          <p class="small text-muted">$250</p>
+                          <h6> <a class="reset-anchor" href="detail.html">Bagus Sikat Pahlawan</a></h6>
+                          <p class="small text-muted">Rp20.000</p>
                       </div>
                   </div>
                   <!-- PRODUCT-->
@@ -166,15 +166,15 @@
                                   </ul>
                               </div>
                           </div>
-                          <h6> <a class="reset-anchor" href="detail.html">Nike air max 95</a></h6>
-                          <p class="small text-muted">$300</p>
+                          <h6> <a class="reset-anchor" href="detail.html">Dettol Soap Lasting Fresh 105g</a></h6>
+                          <p class="small text-muted">Rp5.500</p>
                       </div>
                   </div>
                   <!-- PRODUCT-->
                   <div class="col-xl-3 col-lg-4 col-sm-6">
                       <div class="product text-center">
                           <div class="position-relative mb-3">
-                              <div class="badge text-white badge-"></div><a class="d-block" href="detail.html"><img class="img-fluid w-100" src="<?php echo base_url(); ?>/assets/assets_home/img/product-7.jpg" alt="..."></a>
+                              <div class="badge text-white badge-"></div><a class="d-block" href="detail.html"><img class="img-fluid w-100" src="<?php echo base_url(); ?>/assets/assets_home/img/shampo.png" alt="..."></a>
                               <div class="product-overlay">
                                   <ul class="mb-0 list-inline">
                                       <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#"><i class="far fa-heart"></i></a></li>
@@ -183,15 +183,15 @@
                                   </ul>
                               </div>
                           </div>
-                          <h6> <a class="reset-anchor" href="detail.html">Joemalone Women prefume</a></h6>
-                          <p class="small text-muted">$25</p>
+                          <h6> <a class="reset-anchor" href="detail.html">Head & Shoulders Supreme Shampoo Anti Hair Fall 330 ml</a></h6>
+                          <p class="small text-muted">Rp48.000</p>
                       </div>
                   </div>
                   <!-- PRODUCT-->
                   <div class="col-xl-3 col-lg-4 col-sm-6">
                       <div class="product text-center">
                           <div class="position-relative mb-3">
-                              <div class="badge text-white badge-"></div><a class="d-block" href="detail.html"><img class="img-fluid w-100" src="<?php echo base_url(); ?>/assets/assets_home/img/product-8.jpg" alt="..."></a>
+                              <div class="badge text-white badge-"></div><a class="d-block" href="detail.html"><img class="img-fluid w-100" src="<?php echo base_url(); ?>/assets/assets_home/img/sereal.png" alt="..."></a>
                               <div class="product-overlay">
                                   <ul class="mb-0 list-inline">
                                       <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#"><i class="far fa-heart"></i></a></li>
@@ -200,8 +200,8 @@
                                   </ul>
                               </div>
                           </div>
-                          <h6> <a class="reset-anchor" href="detail.html">Apple Watch</a></h6>
-                          <p class="small text-muted">$351</p>
+                          <h6> <a class="reset-anchor" href="detail.html">Coco Krunch Sereal 300gr</a></h6>
+                          <p class="small text-muted">Rp40.000</p>
                       </div>
                   </div>
               </div>
